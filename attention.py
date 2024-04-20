65066ee77f3a11afdd0f35177cf200cb3205395f
Vishnu Pamula <vpamula@gmail.com>
2024-04-20 01:18:00 -0700

    working on self attention

---
+++ attention.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn 
+from torch.nn import functional as F
+import math
+
+class SelfAttention(nn.Module):
+
+    def __init__(self, n_heads: int, d_embed: int, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+
+        self.in_proj = nn.Linear(d_embed, 3*d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x: torch.Tensor, causal_mask=False):
+        # x: (Batch_size, Seq_len, d_embed)
+
+        batch_size, sequence_length, d_embed = x.shape
+
+        interim_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
+
+        q, k, v = self.in_proj(x).chunk(3, dim=-1)
